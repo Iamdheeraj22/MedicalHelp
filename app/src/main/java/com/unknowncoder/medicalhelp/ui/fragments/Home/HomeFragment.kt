0971120e52f5a72566debeb06b0b2913078e5436
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2022-04-20 23:45:19 +0530

    linking of fragments

--- app/src/main/java/com/unknowncoder/medicalhelp/ui/fragments/Home/HomeFragment.kt
+++ app/src/main/java/com/unknowncoder/medicalhelp/ui/fragments/Home/HomeFragment.kt
@@ -5,14 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.unknowncoder.medicalhelp.R
+import com.unknowncoder.medicalhelp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding= FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
