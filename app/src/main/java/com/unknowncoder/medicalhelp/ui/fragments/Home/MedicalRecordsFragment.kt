7856b04345ce4936f7077b33db38e57145e0d7d4
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2022-04-21 23:07:35 +0530

    add new fragments for medical records

---
+++ app/src/main/java/com/unknowncoder/medicalhelp/ui/fragments/Home/MedicalRecordsFragment.kt
@@ -0,0 +1,18 @@
+package com.unknowncoder.medicalhelp.ui.fragments.Home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.unknowncoder.medicalhelp.R
+class MedicalRecordsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_medical_records, container, false)
+    }
+}
